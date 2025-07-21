5a28e85bce5bee5ea732d221e8977c64fe30c050
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-07-21 14:45:26 -0400

    random is working

--- index.js
+++ index.js
@@ -1,38 +1,79 @@
-let writer = () => {
-  document.write('Hello World');
-};
-
 function randomArtPrimaryImageSmallMaker() {
-  let artPiece = Math.floor((Math.random() * (496907 - 1) +1 ));
+  let artPiece = Math.floor(Math.random() * (496907 - 1) + 1);
   // let artPiece = fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
   var artPieceObjectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artPiece}`;
-  fetch(`${artPieceObjectUrl}`)
-    .then(rawResponse => {
-      if(!rawResponse.ok) {
-        throw new Error('website broke')
-      };
+  return fetch(`${artPieceObjectUrl}`)
+    .then((rawResponse) => {
+      if (!rawResponse.ok) {
+        throw new Error("website broke");
+      }
       return rawResponse.json();
     })
-    .then(jsonifiedResponse => {
+    .then((jsonifiedResponse) => {
       let output = {
         title: jsonifiedResponse.title,
         medium: jsonifiedResponse.medium,
         objectID: jsonifiedResponse.objectID,
-        url: artPieceObjectUrl
-      }
-      console.log(output)
+        url: artPieceObjectUrl,
+        smallPhoto: jsonifiedResponse.primaryImageSmall,
+        department: jsonifiedResponse.department,
+        artist: jsonifiedResponse.artistDisplayName,
+        endDate: jsonifiedResponse.objectEndDate,
+        acquireDate: jsonifiedResponse.accessionYear,
+        dateRange: jsonifiedResponse.objectDate,
+      };
       return output;
+    });
+}
 
+function randParser() {
+  return randomArtPrimaryImageSmallMaker()
+    .then((artObj) => {
+      console.log("randParser: " + artObj.url);
+      return `The piece is '${artObj.title}'`;
     })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
-};
+function aboutParser() {
+  return randomArtPrimaryImageSmallMaker()
+    .then((artObj) => {
+      console.log("aboutParser: " + artObj.url);
+      return (
+        `The piece was made by ${artObj.artist}.` +
+        ` It was acquired by ${artObj.department} in ${artObj.acquireDate}.`
+      );
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
+function photoParser() {
+  return randomArtPrimaryImageSmallMaker().then((artObj) => {
+    console.log("photoParser: " + artObj.url);
+    return artObj.smallPhoto;
+  });
+}
 
-// async function smallImageReader() {
-//   try {
-//     let smallJson = randomArtPrimaryImageSmallMaker()
-//     console.log(smallJson)
-//   } catch (error) {
-//     console.log('async caught error ' + error)
-//   }
-// }
+async function updateTitlePara() {
+  let titleParagraph = await randParser();
+  document.getElementById("titlePara").value = titleParagraph;
+}
+
+async function updateAboutPara() {
+  let aboutParagraph = await aboutParser();
+  document.getElementById("aboutPara").value = aboutParagraph;
+}
+
+async function updatePhoto() {
+  let mainPhoto = await photoParser();
+  console.log(mainPhoto);
+  document.getElementById("mainPhoto").src = mainPhoto;
+}
+
+updateTitlePara();
+updateAboutPara();
+updatePhoto();
