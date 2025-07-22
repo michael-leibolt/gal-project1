36a351b101609db046ab0b01720781804de9249f
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-07-22 15:39:07 -0400

    ValidIdGetter kinda works

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+function validIdGetter() {
+  fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+    .then((rawResponse) => rawResponse.json())
+    .then((jsonifiedResponse) => {
+      let output = {
+        total: jsonifiedResponse.total,
+        Ids: jsonifiedResponse.objectIDs,
+      };
+      console.log("made it here");
+      return output;
+    })
+    .then((output) => {
+      let call = output;
+      let randIndex = Math.floor(Math.random() * (call.total - 1) + 1);
+      let artPiece = randIndex;
+      var artPieceObjectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artPiece}`;
+      console.log("made it to before fetch");
+      fetch(`${artPieceObjectUrl}`)
+        .then((rawResponce) => rawResponce.json())
+        .then((jsonifiedResponse) => {
+          return jsonifiedResponse;
+        });
+    });
+}
+
 function randomArtPrimaryImageSmallMaker() {
   let artPiece = Math.floor(Math.random() * (496907 - 1) + 1);
-  // let artPiece = fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
   var artPieceObjectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artPiece}`;
   return fetch(`${artPieceObjectUrl}`)
     .then((rawResponse) => {
@@ -102,6 +126,95 @@ function randParser() {
 }
 randParser();
 
+function objectIdMaker() {
+  let artPiece = logger();
+  var artPieceObjectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artPiece}`;
+  return fetch(`${artPieceObjectUrl}`)
+    .then((rawResponse) => {
+      if (!rawResponse.ok) {
+        alert("please try again!");
+        throw new Error("website broke");
+      }
+      return rawResponse.json();
+    })
+    .then((jsonifiedResponse) => {
+      let output = {
+        title: jsonifiedResponse.title,
+        medium: jsonifiedResponse.medium,
+        objectID: jsonifiedResponse.objectID,
+        url: artPieceObjectUrl,
+        bigPhoto: jsonifiedResponse.primaryImage,
+        smallPhoto: jsonifiedResponse.primaryImageSmall,
+        department: jsonifiedResponse.department,
+        artist: jsonifiedResponse.artistDisplayName,
+        endDate: jsonifiedResponse.objectEndDate,
+        acquireDate: jsonifiedResponse.accessionYear,
+        dateRange: jsonifiedResponse.objectDate,
+      };
+      //console.log(artPiece);
+      //console.log(output.title);
+      return output;
+    });
+}
+
+function objectIdParser() {
+  return objectIdMaker()
+    .then((artObj) => {
+      if (artObj.title.trim() === "") {
+        document.getElementById("titlePara").innerHTML = "No Title";
+        return artObj;
+      } else {
+        document.getElementById("titlePara").innerHTML = artObj.title;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      if (artObj.smallPhoto.trim() === "" && artObj.bigPhoto.trim() === "") {
+        document.getElementById("mainPhoto").src =
+          "https://image.similarpng.com/file/similarpng/very-thumbnail/2021/06/Art-empty-frame-in-golden-on-transparent-background-PNG.png";
+        document.getElementById("mainPhoto").alt = "Photo does not exist";
+        return artObj;
+      } else if (artObj.smallPhoto.trim() !== "") {
+        document.getElementById("mainPhoto").src = artObj.smallPhoto;
+        return artObj;
+      } else {
+        document.getElementById("mainPhoto").src = artObj.bigPhoto;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      if (artObj.artist.trim() === "") {
+        document.getElementById("tableArtist").innerText = "Unknown Artist";
+        return artObj;
+      } else {
+        document.getElementById("tableArtist").innerText = artObj.artist;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      if (artObj.department.trim() === "") {
+        document.getElementById("tableDepartment").innerText =
+          "Unknown Department";
+        return artObj;
+      } else {
+        document.getElementById("tableDepartment").innerText =
+          artObj.department;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      if (artObj.acquireDate.trim() === "") {
+        document.getElementById("tableDate").innerText = "Unknown Date";
+      } else {
+        document.getElementById("tableDate").innerText = artObj.acquireDate;
+        return artObj;
+      }
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
+}
+
 function addElement() {
   if (finderModes.value === "ObjectId") {
     let objectId = document.createElement("input");
@@ -144,108 +257,4 @@ function logger() {
     //console.log(submitInput);
     return submitNumber;
   }
-}
-
-function objectIdMaker() {
-  let artPiece = logger();
-  var artPieceObjectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artPiece}`;
-  return fetch(`${artPieceObjectUrl}`)
-    .then((rawResponse) => {
-      if (!rawResponse.ok) {
-        alert("please try again!");
-        throw new Error("website broke");
-      }
-      return rawResponse.json();
-    })
-    .then((jsonifiedResponse) => {
-      let output = {
-        title: jsonifiedResponse.title,
-        medium: jsonifiedResponse.medium,
-        objectID: jsonifiedResponse.objectID,
-        url: artPieceObjectUrl,
-        bigPhoto: jsonifiedResponse.primaryImage,
-        smallPhoto: jsonifiedResponse.primaryImageSmall,
-        department: jsonifiedResponse.department,
-        artist: jsonifiedResponse.artistDisplayName,
-        endDate: jsonifiedResponse.objectEndDate,
-        acquireDate: jsonifiedResponse.accessionYear,
-        dateRange: jsonifiedResponse.objectDate,
-      };
-      console.log(artPiece);
-      console.log(output.title);
-      return output;
-    });
-}
-
-function objectIdParser() {
-  return objectIdMaker()
-    .then((artObj) => {
-      //console.log("TitleParser: " + artObj.title);
-      //console.log("Da url: " + artObj.url);
-      if (artObj.title.trim() === "") {
-        //console.log("title does not exist");
-        document.getElementById("titlePara").innerHTML = "No Title";
-        return artObj;
-      } else {
-        document.getElementById("titlePara").innerHTML = artObj.title;
-        return artObj;
-      }
-    })
-    .then((artObj) => {
-      //console.log("photoParser: " + artObj.title);
-      //console.log(artObj.smallPhoto);
-      //console.log(artObj.bigPhoto);
-      if (artObj.smallPhoto.trim() === "" && artObj.bigPhoto.trim() === "") {
-        //console.log("photo does not exist");
-        document.getElementById("mainPhoto").src =
-          "https://image.similarpng.com/file/similarpng/very-thumbnail/2021/06/Art-empty-frame-in-golden-on-transparent-background-PNG.png";
-        document.getElementById("mainPhoto").alt = "Photo does not exist";
-        return artObj;
-      } else if (artObj.smallPhoto.trim() !== "") {
-        document.getElementById("mainPhoto").src = artObj.smallPhoto;
-        return artObj;
-      } else {
-        document.getElementById("mainPhoto").src = artObj.bigPhoto;
-        return artObj;
-      }
-    })
-    .then((artObj) => {
-      //console.log(
-      //   "Artist: " +
-      //     artObj.artist +
-      //     "Department: " +
-      //     artObj.department +
-      //     "Date: " +
-      //     artObj.acquireDate
-      // );
-      if (artObj.artist.trim() === "") {
-        document.getElementById("tableArtist").innerText = "Unknown Artist";
-        return artObj;
-      } else {
-        document.getElementById("tableArtist").innerText = artObj.artist;
-        return artObj;
-      }
-    })
-    .then((artObj) => {
-      if (artObj.department.trim() === "") {
-        document.getElementById("tableDepartment").innerText =
-          "Unknown Department";
-        return artObj;
-      } else {
-        document.getElementById("tableDepartment").innerText =
-          artObj.department;
-        return artObj;
-      }
-    })
-    .then((artObj) => {
-      if (artObj.acquireDate.trim() === "") {
-        document.getElementById("tableDate").innerText = "Unknown Date";
-      } else {
-        document.getElementById("tableDate").innerText = artObj.acquireDate;
-        return artObj;
-      }
-    })
-    .catch((error) => {
-      console.error("Error: ", error);
-    });
 }
