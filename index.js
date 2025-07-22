02c54bd7e8553494d6e53adfd93a07e6b8c5f85d
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-07-22 14:29:28 -0400

    can select an object by Id number

--- index.js
+++ index.js
@@ -111,7 +111,8 @@ function addElement() {
     objectId.id = "objectIdInput";
     objectIdSubmit.innerHTML = "Submit";
     objectIdSubmit.id = "objectIdSubmit";
-    objectIdSubmit.setAttribute("onclick", "logger()");
+    objectIdSubmit.setAttribute("onclick", "objectIdParser()");
+
     document.getElementById("top").after(objectId);
     document.getElementById("objectIdInput").after(objectIdSubmit);
     //console.log("made it to the end");
@@ -128,10 +129,123 @@ const topperButton2 = document.querySelector("#topperButton2");
 
 topperButton2.addEventListener("click", () => randParser());
 finderModes.addEventListener("change", () => {
-  console.log(finderModes.value);
+  //console.log(finderModes.value);
   addElement();
 });
 
 function logger() {
-  console.log(document.getElementById("objectIdInput").value);
+  //console.log(document.getElementById("objectIdInput").value);
+  let submitInput = document.getElementById("objectIdInput").value;
+  let submitNumber = Number(submitInput);
+  //console.log(submitNumber);
+  if (Number.isNaN(submitNumber) === true) {
+    alert("Invalid input. Please input a number");
+  } else {
+    //console.log(submitInput);
+    return submitNumber;
+  }
+}
+
+function objectIdMaker() {
+  let artPiece = logger();
+  var artPieceObjectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artPiece}`;
+  return fetch(`${artPieceObjectUrl}`)
+    .then((rawResponse) => {
+      if (!rawResponse.ok) {
+        alert("please try again!");
+        throw new Error("website broke");
+      }
+      return rawResponse.json();
+    })
+    .then((jsonifiedResponse) => {
+      let output = {
+        title: jsonifiedResponse.title,
+        medium: jsonifiedResponse.medium,
+        objectID: jsonifiedResponse.objectID,
+        url: artPieceObjectUrl,
+        bigPhoto: jsonifiedResponse.primaryImage,
+        smallPhoto: jsonifiedResponse.primaryImageSmall,
+        department: jsonifiedResponse.department,
+        artist: jsonifiedResponse.artistDisplayName,
+        endDate: jsonifiedResponse.objectEndDate,
+        acquireDate: jsonifiedResponse.accessionYear,
+        dateRange: jsonifiedResponse.objectDate,
+      };
+      console.log(artPiece);
+      console.log(output.title);
+      return output;
+    });
+}
+
+function objectIdParser() {
+  return objectIdMaker()
+    .then((artObj) => {
+      //console.log("TitleParser: " + artObj.title);
+      //console.log("Da url: " + artObj.url);
+      if (artObj.title.trim() === "") {
+        //console.log("title does not exist");
+        document.getElementById("titlePara").innerHTML = "No Title";
+        return artObj;
+      } else {
+        document.getElementById("titlePara").innerHTML = artObj.title;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      //console.log("photoParser: " + artObj.title);
+      //console.log(artObj.smallPhoto);
+      //console.log(artObj.bigPhoto);
+      if (artObj.smallPhoto.trim() === "" && artObj.bigPhoto.trim() === "") {
+        //console.log("photo does not exist");
+        document.getElementById("mainPhoto").src =
+          "https://image.similarpng.com/file/similarpng/very-thumbnail/2021/06/Art-empty-frame-in-golden-on-transparent-background-PNG.png";
+        document.getElementById("mainPhoto").alt = "Photo does not exist";
+        return artObj;
+      } else if (artObj.smallPhoto.trim() !== "") {
+        document.getElementById("mainPhoto").src = artObj.smallPhoto;
+        return artObj;
+      } else {
+        document.getElementById("mainPhoto").src = artObj.bigPhoto;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      //console.log(
+      //   "Artist: " +
+      //     artObj.artist +
+      //     "Department: " +
+      //     artObj.department +
+      //     "Date: " +
+      //     artObj.acquireDate
+      // );
+      if (artObj.artist.trim() === "") {
+        document.getElementById("tableArtist").innerText = "Unknown Artist";
+        return artObj;
+      } else {
+        document.getElementById("tableArtist").innerText = artObj.artist;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      if (artObj.department.trim() === "") {
+        document.getElementById("tableDepartment").innerText =
+          "Unknown Department";
+        return artObj;
+      } else {
+        document.getElementById("tableDepartment").innerText =
+          artObj.department;
+        return artObj;
+      }
+    })
+    .then((artObj) => {
+      if (artObj.acquireDate.trim() === "") {
+        document.getElementById("tableDate").innerText = "Unknown Date";
+      } else {
+        document.getElementById("tableDate").innerText = artObj.acquireDate;
+        return artObj;
+      }
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
 }
